48dfd02309f8424394c6970c0ec692089463a13b
omartrese <omarlayachi13@gmail.com>
2024-03-23 17:56:52 +0100

    tailwind add

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <h1>hello world</h1>
+      <div className='bg-gray-500 h-screen'>
+        <header className='flex flex-col items-center p-10'>
+          <h1 className='m-2'>Todo App</h1>
+          <h2>by Omar Layachi Mahboub</h2>
+        </header>
+
+
+        <toDo />
+
+        <footer className='hidden sm:w-full sm:bottom-0 sm:flex sm:justify-around bg-gray-900 text-2xl p-2  sm:absolute'>
+          <h3>Todo App</h3>
+          <h3>By Omar layachi</h3>
+        </footer>
       </div>
     </>
   )
---
+++ src/components/Form.jsx
@@ -0,0 +1,9 @@
+function Form() {
+  return (
+    <div>
+        input
+    </div>
+  )
+}
+
+export default Form
