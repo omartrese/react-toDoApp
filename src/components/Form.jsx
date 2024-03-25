395f34a4ac0d456ddd12604c1a56445dc80f7506
omartrese <omarlayachi13@gmail.com>
2024-03-25 13:57:24 +0100

    input and Storage

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,29 +5,36 @@ function Form() {
   const [title, setTitle] = useState(null);
   const [desc, setDesc] = useState(null);
 
-  const handleChange = (e) => {
-    if(e.target.name === 'title') return setTitle(e.target.value);
-
-    return setDesc(e.target.value);
-  }
+  const getTasks = () => JSON.parse(localStorage.getItem('tasks')) || []; 
+  const setTasks = (tasks) => localStorage.setItem('tasks', JSON.stringify(tasks));
+  
+  const handleChange = (e) => e.target.name === 'title' ? setTitle(e.target.value) : setDesc(e.target.value);
+  const cleanInput = () => {setTitle(''); setDesc('');}
 
   const addTask = (e) => {
     e.preventDefault();
 
-    return console.log( {
+    if(!title) return;
+
+    let tasks = getTasks(); 
+
+    console.log( {
       title,
       desc
-    })
-  }
+    });
 
-  console.log(title);
-  console.log(desc);
+    tasks.push({title, desc});
+    setTasks(tasks);
+
+    cleanInput();
+
+  }
 
   return (
     <div>
-      <form>
-        <input type="text" name="title" placeholder="Title" onChange={handleChange}/>
-        <input type="text" name="desc" placeholder="Description" onChange={handleChange}/>
+      <form className="flex flex-col">
+        <input type="text" name="title" placeholder="Title" onChange={handleChange} value={title} />
+        <input type="text" name="desc" placeholder="Description" onChange={handleChange} value={desc} />
         <input type="submit" value="Create Task" onClick={addTask} />
       </form>
     </div>
--- src/data/data.js
+++
@@ -1,6 +0,0 @@
-localStorage.setItem("todos", JSON.stringify([{title: "test", desc:"testDesc"}]));
-
-export const tasks = () => {
-    const todosFromStorage = localStorage.getItem("todos");
-    return todosFromStorage ? JSON.parse(todosFromStorage) : [];
-}
