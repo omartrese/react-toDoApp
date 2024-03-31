5d4a6fb414a0e1a3d9e16c23e39c82360e9fa593
omartrese <omarlayachi13@gmail.com>
2024-03-31 15:12:22 +0200

    main functionality finished

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -6,15 +6,15 @@ function Task(props) {
   return (
     <div className="flex flex-row justify-around items-center my-10 bg-slate-600">
 
-      <button className="bg-red-600 p-1 rounded-md scale-100 transition duration-75 hover:scale-110 active:scale-90" type="button">
+      <button className="bg-red-600 p-1 rounded-md scale-100 transition duration-75 hover:scale-110 active:scale-90" type="button" onClick={props.onDelete}>
         <Trash />
       </button>
 
-      <div className="w-56 text-center text-2xl bg-complement mx-5 p-3 rounded-md">
+      <div className={`w-56 text-center text-2xl bg-complement mx-5 p-3 rounded-md ${props.completed ? 'line-through' : ''}`}>
         <h1>{props.title}</h1>
       </div>
 
-      <button className="bg-green-600 p-1 rounded-md scale-100 transition duration-75 hover:scale-110 active:scale-90" type="button">
+      <button className="bg-green-600 p-1 rounded-md scale-100 transition duration-75 hover:scale-110 active:scale-90" type="button" onClick={props.onToggle}>
         <Tick />
       </button>
     </div>
--- src/components/TodoCrud.jsx
+++ src/components/TodoCrud.jsx
@@ -17,22 +17,30 @@ function TodoCrud() {
     e.preventDefault();
     if (!title || !title.trim()) return;
 
-    const newTask = { title, id: tasks.length };
+    const newTask = { title, id: tasks.length, completed: false };
     setTasks(prevTasks => [...prevTasks, newTask]);
 
     setTitle('');
   }
 
+  const deleteTask = (id) => {
+    setTasks(tasks.filter(task => task.id !== id));
+  }
+
+  const toggleCompletion = (id) => {
+    setTasks(tasks.map(task => task.id === id ? { ...task, completed: !task.completed } : task));
+  }
+
   const tasksComponents = tasks.map(task =>
     <li className="flex justify-center items-center" key={task.id}>
-      <Task title={task.title} />
+      <Task title={task.title} onDelete={() => deleteTask(task.id)} onToggle={() => toggleCompletion(task.id)} completed={task.completed} />
     </li>
   );
 
   return (
     <div>
       <form className="flex flex-row justify-center">
-        <input type="text" name="title" placeholder="Title" onChange={handleChange} value={title} maxLength={14} autoComplete="off" className="text-2xl p-1 w-60 mx-4 rounded-md bg-contrast" required/>
+        <input type="text" name="title" placeholder="Title" onChange={handleChange} value={title} maxLength={14} autoComplete="off" className="text-2xl p-1 w-60 mx-4 rounded-md bg-contrast" required />
         {/* <input type="text" name="desc" placeholder="Description" onChange={handleChange} value={desc} autoComplete="off" /> */}
         <button type="submit" onClick={addTask} className="rounded-md mx-4 p-2 bg-complement scale-100 transition duration-75 hover:scale-110 active:scale-90">
           <Add />
